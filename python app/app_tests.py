4d79a577b0aace11ea5f1dcb1d844e8a6e10c232
niroshsuthagar-qa <nirosh.suthagar@qa.com>
2025-05-14 10:25:29 +0100

    added app test guidance

---
+++ python app/app_tests.py
@@ -0,0 +1,9 @@
+import unittest
+import pandas as pd
+from app_refactored import #Add your functions that you want to test. 
+
+
+# AT A MINUMUM TEST THESE: 
+## Check that the duration column (between checkout and return) is an integer 
+## Check that the same column is above zero
+## Do any other function you've created (or mine)
